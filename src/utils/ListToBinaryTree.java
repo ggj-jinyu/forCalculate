d642af7a32970bb854262e99479417f2463b423c
GuangJian Gong <2690910127.com>
2021-04-21 20:54:47 +0800

    完成了List<Object>转BinaryTree的toBinaryTree方法

--- src/utils/ListToBinaryTree.java
+++ src/utils/ListToBinaryTree.java
@@ -1,6 +1,7 @@
 package utils;
 
 import javaBean.BinaryTree;
+import javaBean.BinaryTreeNode;
 import javaBean.Fraction;
 
 import java.util.ArrayList;
@@ -11,10 +12,29 @@ public class ListToBinaryTree {
     private ListToBinaryTree() { }
 
     //生成二叉树
-    public static BinaryTree toBinaryTree(List<Object> exp, int range){
-        return null;
+    public static BinaryTree toBinaryTree(List<Object> exp){
+        Stack<BinaryTreeNode> fractionStack = new Stack<>();
+        for (Object o : exp) {
+            BinaryTreeNode node = new BinaryTreeNode(o);
+            //操作数直接入栈
+            //操作符则弹出两个node设置左右子树
+            if(o instanceof String){
+                BinaryTreeNode node1 = fractionStack.pop();
+                BinaryTreeNode node2 = fractionStack.pop();
+                if(node2.max(node1)){
+                    node.setLeftChild(node1);
+                    node.setRightChild(node2);
+                }else{
+                    node.setLeftChild(node2);
+                    node.setRightChild(node1);
+                }
+            }
+            fractionStack.push(node);
+        }
+        return new BinaryTree(fractionStack.pop());
     }
 
+    //算术表达式 中序转后序
     public static List<Object> toPost(List<Object> exp){
         Stack<String> opStack = new Stack<>();
         List<Object> reversePolish = new ArrayList<>();
