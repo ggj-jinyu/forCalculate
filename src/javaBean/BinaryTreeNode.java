bfe7b66472e43201a73d23d91c8d1377cbc9ac8a
GuangJian Gong <2690910127.com>
2021-04-21 20:10:47 +0800

    创建了二叉树相关文件

---
+++ src/javaBean/BinaryTreeNode.java
@@ -0,0 +1,49 @@
+package javaBean;
+
+public class BinaryTreeNode {
+    private Object data;  //数据
+    private BinaryTreeNode leftChild;  //左孩子
+    private BinaryTreeNode rightChild; //右孩子
+
+    public BinaryTreeNode() {
+    }
+
+    public BinaryTreeNode(Object data) {
+        this.data = data;
+    }
+
+    public BinaryTreeNode(Object data, BinaryTreeNode leftChild) {
+        this.data = data;
+        this.leftChild = leftChild;
+    }
+
+    public BinaryTreeNode(Object data, BinaryTreeNode leftChild, BinaryTreeNode rightChild) {
+        this.data = data;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public BinaryTreeNode getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(BinaryTreeNode leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public BinaryTreeNode getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(BinaryTreeNode rightChild) {
+        this.rightChild = rightChild;
+    }
+}
