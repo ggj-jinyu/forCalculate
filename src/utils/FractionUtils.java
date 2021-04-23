fe989b72dece4076f8e7783d22af9ab6026063de
GuangJian Gong <2690910127.com>
2021-04-23 19:33:37 +0800

    Update FractionUtils.java

--- src/utils/FractionUtils.java
+++ src/utils/FractionUtils.java
@@ -7,6 +7,7 @@ public class FractionUtils {
 
     //Fraction的加法运算
     public static Fraction add(Fraction f1, Fraction f2){
+        if(f1==null||f2==null) return null;
         int denominator = f1.getDenominator() * f2.getDenominator();
         int nominator = f1.getNumerator() * f2.getDenominator()
                 + f1.getDenominator() * f2.getNumerator();
@@ -15,6 +16,7 @@ public class FractionUtils {
 
     //Fraction的减法运算
     public static Fraction subtract(Fraction f1, Fraction f2){
+        if(f1==null||f2==null) return null;
         //生成的题目中计算过程不能产生负数，若为负数则返回null
         if(f2.max(f1)) return null;
         else {
@@ -27,6 +29,7 @@ public class FractionUtils {
 
     //Fraction的乘法运算
     public static Fraction multiply(Fraction f1, Fraction f2){
+        if(f1==null||f2==null) return null;
         int nominator = f1.getNumerator() * f2.getNumerator();
         int denominator = f1.getDenominator() * f2.getDenominator();
         return new Fraction(nominator,denominator);
@@ -34,6 +37,7 @@ public class FractionUtils {
 
     //Fraction的乘法运算
     public static Fraction divide(Fraction f1, Fraction f2){
+        if(f1==null||f2==null) return null;
         int nominator = f1.getNumerator() * f2.getDenominator();
         int denominator = f2.getNumerator() * f1.getDenominator();
         if(denominator == 0) return null;
